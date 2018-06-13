cf6b1df563f100cfb57c010cfae2743a1fad8c31
Jordan Harris <jharris@wework.com>
2018-06-13 17:34:08 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,9 @@
 
 
 def position_taken?(board, index)
-  if ! (board[index] == " " || board[index] == "" || board[index] == nil)
-  else 
-    true
+  if !(board[index] == " " || board[index] == "" || board[index] == nil)
+  # else 
+  #   true
   end
 end
 
