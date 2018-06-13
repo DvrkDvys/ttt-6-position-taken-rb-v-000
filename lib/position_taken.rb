04029e2974a256ab1b426f649187e4db6f984128
Jordan Harris <jharris@wework.com>
2018-06-13 17:35:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -10,17 +10,3 @@ def position_taken?(board, index)
 end
 
 
-
-
-
-
-#   it 'returns true if the board has a value of "X" or "O" in the index' do
-#     board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
-
-#     index = 0
-#     expect(position_taken?(board, index)).to be(true)
-
-#     index = 8
-#     expect(position_taken?(board, index)).to be(true)
-#   end
-# end
